ab605cc7170227a47a1168953a3589691ce0c402
AmSmo <adam.smolenski@gmail.com>
2020-08-13 15:46:57 -0400

    round 2 done

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,6 @@
 class Teacher < ActiveRecord::Base
-
+    has_many :students
     def tenure
-        self.years_of_experience >5
+        self.years_of_experience > 5
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
-Student.create(first_name: "Adam", last_name: "Smolenski", grade_level: "first")
-Student.create(first_name: "Alex", last_name: "Neumann", grade_level: "second")
-Student.create(first_name: "Beth", last_name: "Lake", grade_level: "third")
-Student.create(first_name: "Charles", last_name: "Shell", grade_level: "third")
+Teacher.destroy_all
+Student.destroy_all
+second = Teacher.create(last_name: "Resnick", grade_level:"second", years_of_experience: 4)
+third = Teacher.create(last_name: "Maskin", grade_level:"third", years_of_experience: 3)
+first = Teacher.create(last_name: "Rogavin", grade_level:"first", years_of_experience: 20)
+fourth = Teacher.create(last_name: "Kaplan", grade_level:"fourth", years_of_experience: 23)
 
-
-Teacher.create(last_name: "Resnick", grade_level:"second", years_of_experience: 4)
-Teacher.create(last_name: "Maskin", grade_level:"third", years_of_experience: 3)
-Teacher.create(last_name: "Rogavin", grade_level:"first", years_of_experience: 20)
-Teacher.create(last_name: "Kaplan", grade_level:"fourt", years_of_experience: 23)
+Student.create(first_name: "Adam", last_name: "Smolenski", grade_level: "first", teacher_id: first.id)
+Student.create(first_name: "Alex", last_name: "Neumann", grade_level: "second", teacher_id: second.id)
+Student.create(first_name: "Beth", last_name: "Lake", grade_level: "third", teacher_id: third.id)
+Student.create(first_name: "Charles", last_name: "Shell", grade_level: "third", teacher_id: third.id)
